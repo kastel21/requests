833d83a3483f9483ed9dbc49b6d08690e5a583ea
kastel21 <kastel21@outlook.com>
2023-09-21 20:45:46 +0200

    adding po page approver

--- home/views.py
+++ home/views.py
@@ -1396,7 +1396,7 @@ def comp_schedule_get_record(request):
       "pdf3":pdf.quote3_path,
 
       "message":"success",
-      "request_id":record.request_id,
+      "request_id":record.service_request,
 
       "company_name_supplier1":record.company_name_supplier1,
       "item_number_supplier1":record.item_number_supplier1,
