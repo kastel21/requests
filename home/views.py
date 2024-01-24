1972caf554208c3c6173d074207060bd44701e28
kastel21 <kastel21@outlook.com>
2024-01-24 12:47:11 +0200

    adding communication btwn fields

--- home/views.py
+++ home/views.py
@@ -3095,7 +3095,7 @@ def get_suppliers(request):
       schedules = Supplier.objects.filter(~Q(approved_by_date = "None"))
 
       for schedule in schedules: 
-          dic[schedule.id]= schedule.item_name+", Total Cost : "+ schedule.total_cost +", raised by : "+ schedule.compiled_by+", raised on : "+schedule.date+", Supplier : "+schedule.sup_name
+          dic[schedule.id]= schedule.name+", Services : "+ schedule.meta_data 
       return JsonResponse(dic)
    except Exception as e:
           return JsonResponse(str(e)) 
