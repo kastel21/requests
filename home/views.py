a1ad0993d80122ca9d50d0a060770de3f2a3ea5f
kastel21 <kastel21@outlook.com>
2024-01-29 10:41:18 +0200

    adding communication

--- home/views.py
+++ home/views.py
@@ -16,8 +16,7 @@ from .utils import *
 from django.contrib.auth.decorators import login_required
 import qrcode
 from django.contrib.auth.models import User
-import time
-
+from time import sleep
 # Data to be encoded
 data = 'QR Code using make() function'
  
@@ -48,7 +47,7 @@ def welcome(request):
     for user in users:
           message = "Good day "+user.username+",\n We would like to welcome you to our procurement system (Berry) your temporary password is test123? to login use your username and this password. Use the following link to access the system.\n https://lorkas.co.zw/procurement/ \nShould you face any challenges kindly contact IT at etakawengwa@brti.co.zw.\n\n regards IT"
           send_notice(message,"system",user.email)
-          time.sleep(60)
+          sleep(60)
 
 
 
